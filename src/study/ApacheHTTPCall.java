04955abc9c0d7bc88027805b2cd12e2f512eb336
YJI <jay96@intellivix.com>
2022-10-18 16:40:49 +0900

    FIX : diva 서버에 붙어서 데이터 받기

--- src/study/ApacheHTTPCall.java
+++ src/study/ApacheHTTPCall.java
@@ -9,6 +9,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -17,37 +18,35 @@ import org.json.simple.JSONObject;
 import java.io.IOException;
 
 public class ApacheHTTPCall {
-//    private static final String GET_URL = "http://localhost:8090/api/pom/v1/stat/history/task?toDate=2022-09-29T00:00:00&fromDate=2022-09-16T00:00:00";
-    private static final String GET_URL = "http://localhost:8888/student/search";
-    private static final String POST_URL = "http://localhost:8090/api/pom/v1/data/group/add";
-//    private static final String POST_URL = "http://localhost:8888/student/update";
 
-    public static void main(String[] args) throws IOException {
+    public static String URL;
+    public void setURL(String url){URL = url;}
+    private static final String GET_URL = "http://localhost:8090/api/pom/v1/stat/history/task?toDate=2022-09-29T00:00:00&fromDate=2022-09-16T00:00:00";
+    private static final String POST_URL = "http://localhost:8090/api/pom/v1/data/group/add";
+
+/*    public static void main(String[] args) throws IOException {
         getRequest();
         System.out.println("GET DONE\n\n");
         postRequest();
         System.out.println("POST DONE\n\n");
         putRequest();
         System.out.println("PUT DONE\n\n");
-        putStudent();
-        System.out.println("PUT DONE\n\n");
-    }
+    }*/
 
-    private static void getRequest() throws ClientProtocolException, IOException {
+    public void getRequest() throws ClientProtocolException, IOException {
         // timeout config 설정
         RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(3000) // the time to establish the connection with the remote host :: 호스츠 서버에 소켓 연결 시 타임아웃
-                .setConnectionRequestTimeout(3000) // the time to wait for a connection from the connection manager/pool :: 커넥션풀과의 연결 시 타임아웃
-                .setSocketTimeout(3000) // the time waiting for data – after establishing the connection; maximum time of inactivity between two data packets :: 요청/응답 사이의 타임아웃
+                .setConnectTimeout(3000) // 호스트 서버에 소켓 연결 시 타임아웃 :: the time to establish the connection with the remote host
+                .setConnectionRequestTimeout(3000) // 커넥션 풀과의 연결 시 타임아웃 :: the time to wait for a connection from the connection manager/pool
+                .setSocketTimeout(3000) // 요청/응답 사이의 타임아웃 :: the time waiting for data – after establishing the connection; maximum time of inactivity between two data packets
                 .build();
 
         // 설정한 config를 적용한 HttpClient 생성
         CloseableHttpClient httpclient = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
-        String responseBody = null;
         try {
             // HTTP GET method
             // 1) 헤더 세팅
-            HttpGet httpget = new HttpGet(GET_URL);
+            HttpGet httpget = new HttpGet(URL);
             httpget.setHeader("Accept", "*/*");
             httpget.setHeader("Content-type", "application/json");
             httpget.setHeader("CLIENT-TYPE", "CLIENT");
@@ -56,13 +55,13 @@ public class ApacheHTTPCall {
             System.out.println("========================================= Executing request =========================================");
             System.out.println("Executing request " + httpget.getRequestLine());
 
-            // 3) Response Handler 사용해서 결과 처리
+            // 3-1) Response Handler 사용해서 결과 처리
             ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
                 @Override
                 public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
                     int status = response.getStatusLine().getStatusCode(); // response 에서 결과 Code값 가져오기
-                    if (status >= 200 && status < 300) {
-                        HttpEntity entity = response.getEntity(); // 200대면 결과 내용 가져오기
+                    if (status == 200) {
+                        HttpEntity entity = response.getEntity(); // 200이면 결과 내용 가져오기
                         return entity != null ? EntityUtils.toString(entity) : null; // 내용이 있으면 string 으로 변환해서 보내고 아니면 null return
                     } else {
                         throw new ClientProtocolException("Errored Response Status: " + status);
@@ -70,10 +69,11 @@ public class ApacheHTTPCall {
                 }
             };
 
-            // 4) responseBody 만들어서 콘솔에 출력
-            responseBody = httpclient.execute(httpget, responseHandler);
-            System.out.println("\n==================================== Response Body ====================================");
+            // 3-2) responseBody 만들어서 콘솔에 출력
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("========================================= Response Body ====================================");
             System.out.println(responseBody);
+
         } catch (ClientProtocolException e) {
             e.printStackTrace();
             System.err.println(e.toString());
@@ -87,7 +87,7 @@ public class ApacheHTTPCall {
     // get 요청 종료
 
     // post 요청 시작
-    public static void postRequest() throws IOException{
+    public void postRequest(String json) throws IOException{
         RequestConfig config = RequestConfig.custom()
                 .setConnectTimeout(3000)
                 .setConnectionRequestTimeout(3000)
@@ -101,53 +101,35 @@ public class ApacheHTTPCall {
             httpPost.setHeader("CLIENT-TYPE", "CLIENT");
             httpPost.setHeader("DIVA-AUTH", "IVX-1234567890");
 
-            // RequestParams 세팅
-            /*List<NameValuePair> params = new ArrayList<>();
-            params.add(new BasicNameValuePair("grpType", "C-GRP"));
-            params.add(new BasicNameValuePair("grpName", "Apache-POSTTest10"));
-            params.add(new BasicNameValuePair("grpParentId", "25"));
-            params.add(new BasicNameValuePair("grpPlaceAddr", "방배 4동 390(C-GRP)"));
-            params.add(new BasicNameValuePair("grpPlaceControlAreaCode", "A-011(C-GRP)"));
-            params.add(new BasicNameValuePair("grpIndustrialSafetySetting", "1"));
-            params.add(new BasicNameValuePair("grpIndustrialSafetySettingImage", " TestImage0010101010(GRP) "));
-            httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8")); // 문자 인코딩 설정*/
-
             // RequestBody 세팅
-            String json = "{\r\n" +
+            /*String json = "{\r\n" +
                     "  \"grpType\": \"C-GRP\",\r\n" +
-                    "  \"grpName\": \"Apache-POSTTest06\",\r\n" +
+                    "  \"grpName\": \"Apache-POSTTest09\",\r\n" +
                     "  \"grpParentId\": \"25\",\r\n" +
-                    "  \"grpPlaceAddr\": \"방배 4동 390(C-GRP)\",\r\n" +
-                    "  \"grpPlaceControlAreaCode\": \"A-011(C-GRP)\",\r\n" +
+                    "  \"grpPlaceAddr\": \"POST_TEST\",\r\n" +
+                    "  \"grpPlaceControlAreaCode\": \"POST_TEST\",\r\n" +
                     "  \"grpIndustrialSafetySetting\": \"1\",\r\n" +
                     "  \"grpIndustrialSafetySettingImage\": \"TestImage0010101010(GRP)\"\r\n" +
-                    "}";
+                    "}";*/
             StringEntity stringEntity = new StringEntity(json, "UTF-8");
             httpPost.setEntity(stringEntity);
 
             System.out.println("========================================= Executing request =========================================");
             System.out.println("Executing request " + httpPost.getRequestLine());
 
-            // Response Handler 만들기
-            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
-                @Override
-                public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
-                    int status = response.getStatusLine().getStatusCode();
-                    if (status >= 200 && status < 300) {
-                        HttpEntity entity = response.getEntity();
-                        return entity != null ? EntityUtils.toString(entity) : null;
-                    } else {
-                        throw new ClientProtocolException("Errored Response Status: " + status);
-                    }
-                }
-            };
-            Object responseBody = httpclient.execute(httpPost, responseHandler);
-            System.out.println("==================================== Response Body ====================================");
-            System.out.println(responseBody);
+            HttpResponse response = httpclient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                ResponseHandler<String> handler = new BasicResponseHandler();
+                String body = handler.handleResponse(response);
+                System.out.println("========================================= Response Body ====================================");
+                System.out.println(body);
+            } else {
+                System.out.println("response is error : " + response.getStatusLine().getStatusCode());
+            }
         }
     }
 
-    public static void putRequest() throws IOException {
+    public void putRequest(String json) throws IOException {
         RequestConfig config = RequestConfig.custom()
                 .setConnectTimeout(3000)
                 .setConnectionRequestTimeout(3000)
@@ -161,16 +143,6 @@ public class ApacheHTTPCall {
             httpPut.setHeader("CLIENT-TYPE", "CLIENT");
             httpPut.setHeader("DIVA-AUTH", "IVX-1234567890");
 
-            String json = "{\r\n" +
-                    "  \"grpType\": \"C-GRP\",\r\n" +
-                    "  \"grpName\": \"Apache_PUT_Test02\",\r\n" +
-                    "  \"grpPk\": \"463\",\r\n" +
-                    "  \"grpPlaceAddr\": \"강남대로 100\",\r\n" +
-                    "  \"grpPlaceControlAreaCode\": \"B-1004(C-GRP)\",\r\n" +
-                    "  \"grpServiceFk\": \"SERVICE-000081\",\r\n" +
-                    "  \"grpIndustrialSafetySetting\": \"0\",\r\n" +
-                    "  \"grpIndustrialSafetySettingImage\": \"TestImage0010101010(GRP)\"\r\n" +
-                    "}";
             StringEntity stringEntity = new StringEntity(json,"UTF-8");
             httpPut.setEntity(stringEntity);
 
@@ -187,55 +159,20 @@ public class ApacheHTTPCall {
                 }
             };
             String responseBody = httpclient.execute(httpPut, responseHandler);
-            System.out.println("==================================== Response Body ====================================");
+            System.out.println("========================================= Response Body ====================================");
             System.out.println(responseBody);
         }
     }
 
-
-    public static void putStudent() throws IOException {
-        RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(3000)
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(30000)
-                .build();
-
-        try (CloseableHttpClient httpclient = HttpClientBuilder.create().setDefaultRequestConfig(config).build()) {
-            HttpPut httpPut = new HttpPut("http://localhost:8888/student/update");
-            httpPut.setHeader("Accept", "*/*");
-            httpPut.setHeader("Content-type", "application/json");
-            httpPut.setHeader("CLIENT-TYPE", "CLIENT");
-            httpPut.setHeader("DIVA-AUTH", "IVX-1234567890");
-
-            String json = "{\r\n" +
-                    "  \"studentName\": \"정충낭\",\r\n" +
-                    "  \"changeName\": \"김한별\",\r\n" +
-                    "  \"korean\": \"100\",\r\n" +
-                    "  \"english\": \"100\",\r\n" +
-                    "  \"math\": \"100\",\r\n" +
-                    "  \"history\": \"100\",\r\n" +
-                    "  \"science\": \"100\",\r\n" +
-                    "}";
-            StringEntity stringEntity = new StringEntity(json,"UTF-8");
-            httpPut.setEntity(stringEntity);
-
-            System.out.println("========================================= Executing request =========================================");
-            System.out.println("Executing request " + httpPut.getRequestLine());
-
-            ResponseHandler<String> responseHandler = response -> {
-                int status = response.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = response.getEntity();
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } else {
-                    throw new ClientProtocolException("Errored Response Status: " + status);
-                }
-            };
-            String responseBody = httpclient.execute(httpPut, responseHandler);
-            System.out.println("==================================== Response Body ====================================");
-            System.out.println(responseBody);
-        }
-    }
-
+    // RequestParams 세팅
+    /*List<NameValuePair> params = new ArrayList<>();
+    params.add(new BasicNameValuePair("grpType", "C-GRP"));
+    params.add(new BasicNameValuePair("grpName", "Apache-POSTTest10"));
+    params.add(new BasicNameValuePair("grpParentId", "25"));
+    params.add(new BasicNameValuePair("grpPlaceAddr", "방배 4동 390(C-GRP)"));
+    params.add(new BasicNameValuePair("grpPlaceControlAreaCode", "A-011(C-GRP)"));
+    params.add(new BasicNameValuePair("grpIndustrialSafetySetting", "1"));
+    params.add(new BasicNameValuePair("grpIndustrialSafetySettingImage", " TestImage0010101010(GRP) "));
+    httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8")); // 문자 인코딩 설정*/
 
 }
--- src/study/study1.java
+++ src/study/study1.java
@@ -1,12 +1,86 @@
 package study;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class study1 {
-    private ApacheHTTPCall apacheHTTPCall;
-
-    public static void main(String[] args) {
-        System.out.println("헬로월드");
-
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ApacheHTTPCall apacheHTTPCall = new ApacheHTTPCall();
 
 
+        while (true){
+            System.out.println("\n어떤 요청을 보내시겠습니까?");
+            String select = scanner.next();
+            switch (select){
+
+                case "get" :
+                    System.out.println("get 요청을 불러옵니다.");
+                    apacheHTTPCall.setURL("http://localhost:8090/api/pom/v1/stat/history/error?toDate=2022-09-29T00:00:00&fromDate=2022-09-16T00:00:00");
+                    apacheHTTPCall.getRequest(); // stat/history/task
+                    break;
+
+                case "post" :
+                    System.out.println("grpType 을 입력하세요.");
+                    String grpType = scanner.next();
+                    System.out.println("grpName 을 입력하세요.");
+                    String grpName = scanner.next();
+                    System.out.println("grpParentId 을 입력하세요.");
+                    String grpParentId = scanner.next();
+                    System.out.println("grpPlaceAddr 을 입력하세요.");
+                    String grpPlaceAddr = scanner.next();
+                    System.out.println("grpPlaceControlAreaCode 을 입력하세요.");
+                    String grpPlaceControlAreaCode = scanner.next();
+                    System.out.println("grpIndustrialSafetySetting 을 입력하세요.");
+                    String grpIndustrialSafetySetting = scanner.next();
+                    System.out.println("grpIndustrialSafetySettingImage 을 입력하세요.");
+                    String grpIndustrialSafetySettingImage = scanner.next();
+
+                    String postJson =
+                            "{\r\n" +
+                                    "  \"grpType\": \""+grpType+"\",\r\n" +
+                                    "  \"grpName\": \""+grpName+"\",\r\n" +
+                                    "  \"grpParentId\": \""+grpParentId+"\",\r\n" +
+                                    "  \"grpPlaceAddr\": \""+grpPlaceAddr+"\",\r\n" +
+                                    "  \"grpPlaceControlAreaCode\": \""+grpPlaceControlAreaCode+"\",\r\n" +
+                                    "  \"grpIndustrialSafetySetting\": \""+grpIndustrialSafetySetting+"\",\r\n" +
+                                    "  \"grpIndustrialSafetySettingImage\": \""+grpIndustrialSafetySettingImage+"\"\r\n" +
+                                    "}";
+                    apacheHTTPCall.postRequest(postJson); // data/group/add
+                    break;
+
+                case "put" :
+                    System.out.println("grpType 을 입력하세요.");
+                    String grpType2 = scanner.next();
+                    System.out.println("grpName 을 입력하세요.");
+                    String grpName2 = scanner.next();
+                    System.out.println("grpPk 을 입력하세요.");
+                    String grpPk = scanner.next();
+                    System.out.println("grpPlaceAddr 을 입력하세요.");
+                    String grpPlaceAddr2 = scanner.next();
+                    System.out.println("grpPlaceControlAreaCode 을 입력하세요.");
+                    String grpPlaceControlAreaCode2 = scanner.next();
+                    System.out.println("grpServiceFk 을 입력하세요.");
+                    String grpServiceFk = scanner.next();
+                    System.out.println("grpIndustrialSafetySetting 을 입력하세요.");
+                    String grpIndustrialSafetySetting2 = scanner.next();
+                    System.out.println("grpIndustrialSafetySettingImage 을 입력하세요.");
+                    String grpIndustrialSafetySettingImage2 = scanner.next();
+
+                    String putJson =
+                            "{\r\n" +
+                                    "  \"grpType\": \""+grpType2+"\",\r\n" +
+                                    "  \"grpName\": \""+grpName2+"\",\r\n" +
+                                    "  \"grpPk\": \""+grpPk+"\",\r\n" +
+                                    "  \"grpPlaceAddr\": \""+grpPlaceAddr2+"\",\r\n" +
+                                    "  \"grpPlaceControlAreaCode\": \""+grpPlaceControlAreaCode2+"\",\r\n" +
+                                    "  \"grpServiceFk\": \""+grpServiceFk+"\",\r\n" +
+                                    "  \"grpIndustrialSafetySetting\": \""+grpIndustrialSafetySetting2+"\",\r\n" +
+                                    "  \"grpIndustrialSafetySettingImage\": \""+grpIndustrialSafetySettingImage2+"\"\r\n" +
+                                    "}";
+                    apacheHTTPCall.putRequest(putJson); // data/group/update
+                    break;
+            }
+        }
     }
 }
